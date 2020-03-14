ebd0a2471f47a932447fb73e2f015005d9c45dce
M-Rodrigues <matheus.rodrigues.ime@gmail.com>
2020-03-14 18:34:23 -0300

    wip

--- server.py
+++ server.py
@@ -19,18 +19,26 @@ def sensor_data(sid, data):
 def echo(sid, data):
     print(sid, data)
     t = time.time()
-    sio.emit('echo_ack', [t, t - data[0]])
+    sio.emit('echo_ack', [t, t - data[0]], room=sid)
 
 @sio.event
 def disconnect(sid):
     print('user disconnected', sid)
 
+@sio.event
+def echo_ack(data):
+	t = time.time()
+	print([t, 1000.0 * (t - data[0]), 1000.0 * data[1]])
+	# sio.emit('echo', [time.time()])
+
 if __name__ == '__main__':
     pid = os.fork()
     if pid == 0:
-        print('HEY')
-        time.sleep(1)
-        print('HEEEEEEY')
-        exit()
+        client = socketio.Client()
+
+        while True:
+            print('ON CLIENT')
+            client.emit('echo', [time.time()])
+            time.sleep(1)
     else:
         eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
