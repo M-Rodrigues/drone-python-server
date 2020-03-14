8f782c39b129960de519074a3d43846f6d8b2fda
M-Rodrigues <matheus.rodrigues.ime@gmail.com>
2020-03-14 18:49:12 -0300

    multiprocess

--- server.py
+++ server.py
@@ -3,10 +3,35 @@ import socketio
 import os
 import time
 
-sio = socketio.Server(cors_allowed_origins='*')
-app = socketio.WSGIApp(sio)
+if __name__ == '__main__':
+    pid = os.fork()
+    if pid == 0:
+        client = socketio.Client()
 
-@sio.event
+        @client.event
+def echo_ack(data):
+	t = time.time()
+	print([t, 1000.0 * (t - data[0]), 1000.0 * data[1]])
+	# sio.emit('echo', [time.time()])
+
+
+        connected = False
+        while not connected:
+            try:
+            	client.connect('http://localhost:5000')
+            	connected = True
+            except:
+            	connected = False
+
+        while True:
+            # print('ON CLIENT')
+            client.emit('echo', [time.time()])
+            time.sleep(1)
+    else:
+        sio = socketio.Server(cors_allowed_origins='*')
+        app = socketio.WSGIApp(sio)
+
+        @sio.event
 def connect(sid, environ):
     print('a user connected', sid)
     sio.emit('sensor_data_ack', room=sid)
@@ -17,28 +42,11 @@ def sensor_data(sid, data):
 
 @sio.event
 def echo(sid, data):
-    print(sid, data)
     t = time.time()
-    sio.emit('echo_ack', [t, t - data[0]], room=sid)
+    sio.emit('echo_ack', [t, t - data[0]])
 
 @sio.event
 def disconnect(sid):
     print('user disconnected', sid)
 
-@sio.event
-def echo_ack(data):
-	t = time.time()
-	print([t, 1000.0 * (t - data[0]), 1000.0 * data[1]])
-	# sio.emit('echo', [time.time()])
-
-if __name__ == '__main__':
-    pid = os.fork()
-    if pid == 0:
-        client = socketio.Client()
-
-        while True:
-            print('ON CLIENT')
-            client.emit('echo', [time.time()])
-            time.sleep(1)
-    else:
         eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
