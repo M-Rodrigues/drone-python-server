c840f935f317f647628527c1dd5ce7981c09f52e
M-Rodrigues <matheus.rodrigues.ime@gmail.com>
2020-03-14 22:41:43 -0300

    connected event

--- app.py
+++ app.py
@@ -5,41 +5,34 @@ import socketio
 import eventlet
 
 def start_drone(q):
-	state = 0.0
 	while True:
 		if not q.empty():
 			state = q.get()
-			print('State processed at', time.time())
-		print('State: {}'.format(state))
+			print('Orientation state updated', state)
 
 def start_server(q):
-	random.seed(0)
-	while True:
-		n = random.randint(1, 10)
-		q.put(n)
-		print('State received at', time.time())
-		time.sleep(random.random() / 10)
-
 	sio = socketio.Server(cors_allowed_origins='*')
 	app = socketio.WSGIApp(sio)
 
 	@sio.event
 	def connect(sid, environ):
-			print('a user connected', sid)
-			sio.emit('sensor_data_ack', room=sid)
+		print('a user connected', sid)
+		sio.emit('connected', room=sid)
 
 	@sio.event
 	def sensor_data(sid, data):
-			sio.emit('sensor_data_ack')
+		if not q.empty():
+			q.get()
+		q.put(data)
+		sio.emit('sensor_data_ack')
 
 	@sio.event
 	def echo(sid, data):
-			t = time.time()
-			sio.emit('echo_ack', [t, t - data[0]])
+		sio.emit('echo_ack', data)
 
 	@sio.event
 	def disconnect(sid):
-			print('user disconnected', sid)
+		print('user disconnected', sid)
 
 	eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
 
