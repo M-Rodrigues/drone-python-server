1ba999459e4646dc61ca8bcdbf2133c3af4e2aaf
M-Rodrigues <matheus.rodrigues.ime@gmail.com>
2020-03-14 22:04:21 -0300

    refactoring

--- app.py
+++ app.py
@@ -1,9 +1,47 @@
 import time
 import multiprocessing
 import random
+import socketio
+import eventlet
 
-from drone import start_drone
-from server import start_server
+def start_drone(q):
+	state = 0.0
+	while True:
+		if not q.empty():
+			state = q.get()
+			print('State processed at', time.time())
+		print('State: {}'.format(state))
+
+def start_server(q):
+	random.seed(0)
+	while True:
+		n = random.randint(1, 10)
+		q.put(n)
+		print('State received at', time.time())
+		time.sleep(random.random() / 10)
+
+	sio = socketio.Server(cors_allowed_origins='*')
+	app = socketio.WSGIApp(sio)
+
+	@sio.event
+	def connect(sid, environ):
+			print('a user connected', sid)
+			sio.emit('sensor_data_ack', room=sid)
+
+	@sio.event
+	def sensor_data(sid, data):
+			sio.emit('sensor_data_ack')
+
+	@sio.event
+	def echo(sid, data):
+			t = time.time()
+			sio.emit('echo_ack', [t, t - data[0]])
+
+	@sio.event
+	def disconnect(sid):
+			print('user disconnected', sid)
+
+	eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
 
 if __name__ == "__main__": 
 	q = multiprocessing.Queue() 
