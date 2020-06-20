21dd07f182ac9b39f7b1d1349ff6aec388e24790
M-Rodrigues <matheus.rodrigues.ime@gmail.com>
2020-06-20 01:33:15 -0300

    wip

--- remote.py
+++ remote.py
@@ -7,14 +7,14 @@ class AndroidRemote:
   def __init__(self):
     self.ser = ser
   
-  def _read_data():
+  def _read_data(self):
     payload = ser.readline()
     return json.loads(payload)
   
-  def _send_ack():
+  def _send_ack(self):
     ser.write(b'ACK\t')
 
-  def get_data():
+  def get_data(self):
     orientation = self._read_data()
     self._send_ack()
     return orientation
