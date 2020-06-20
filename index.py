6808e71e507ecfb3f108b26d7f03fc5783e236c2
M-Rodrigues <matheus.rodrigues.ime@gmail.com>
2020-06-20 00:38:04 -0300

    communication protocol

--- index.py
+++ index.py
@@ -6,7 +6,10 @@ ser = serial.Serial('/dev/rfcomm0', 9600)
 payload = ''
 orient = {}
 
+print('Ready')
 while True:
   payload = ser.readline()
+  # print(payload)
   orient = json.loads(payload)
   print(orient)
+  ser.write(b'ACK\t')
