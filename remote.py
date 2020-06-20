5ad07b135e38e71c7cf638d3d88fe3b3b9326b80
M-Rodrigues <matheus.rodrigues.ime@gmail.com>
2020-06-20 01:33:41 -0300

    wip

--- remote.py
+++ remote.py
@@ -14,7 +14,7 @@ class AndroidRemote:
   def _send_ack(self):
     ser.write(b'ACK\t')
 
-  def get_data(self):
+  def get_data(self ):
     orientation = self._read_data()
     self._send_ack()
     return orientation
